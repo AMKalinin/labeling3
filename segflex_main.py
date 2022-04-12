5cb322974f63a781085ddcd643e5279dc9b62e43
iakhmetev <bsadie@student.21-school.ru>
2022-04-12 12:29:46 +0300

    description вынесен из main

--- segflex_main.py
+++ segflex_main.py
@@ -22,52 +22,12 @@ import time
 import re
 import cv2
 
-
-class project_description(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.file_name = 'не задано'
-        self.file_classes = 'не задано'
-        self.file_time_c = 'не задано'
-        self.file_time_u = 'не задано'
-        self.file_description = 'не задано'
-        self.file_task_count = 'не задано'
-        self.item_name = QListWidgetItem(classifier.HDF_FILE_NAME + self.file_name)
-        self.item_classes = QListWidgetItem(classifier.HDF_FILE_CLASSES + self.file_classes)
-        self.item_time_c = QListWidgetItem(classifier.HDF_FILE_TIME_C + self.file_time_c)
-        self.item_time_u = QListWidgetItem(classifier.HDF_FILE_TIME_U + self.file_time_u)
-        self.item_description = QListWidgetItem(classifier.HDF_FILE_DESCRIPTION + self.file_description)
-        self.item_task_count = QListWidgetItem(classifier.HDF_FILE_TASK_COUNT + self.file_task_count)
-        self.items_menu = QListWidget()
-        self.items_menu.addItem(self.item_name)
-        self.items_menu.addItem(self.item_classes)
-        self.items_menu.addItem(self.item_time_c)
-        self.items_menu.addItem(self.item_time_u)
-        self.items_menu.addItem(self.item_description)
-        self.items_menu.addItem(self.item_task_count)
-        layout = QHBoxLayout()
-        layout.addWidget(self.items_menu)
-        self.setLayout(layout)
-
-    def update_description(self, hdf):
-        self.project_path = project_path
-        with h5py.File(project_path, 'r') as hdf:
-            self.file_name = hdf.attrs[classifier.HDF_FILE_NAME]
-            self.file_classes = hdf.attrs[classifier.HDF_FILE_CLASSES]
-            self.file_time_c = hdf.attrs[classifier.HDF_FILE_TIME_C]
-            self.file_time_u = hdf.attrs[classifier.HDF_FILE_TIME_U]
-            self.file_description = hdf.attrs[classifier.HDF_FILE_DESCRIPTION]
-            self.file_task_count = hdf.attrs[classifier.HDF_FILE_TASK_COUNT]
-        self.item_name.setText(classifier.HDF_FILE_NAME + self.file_name)
-        #self.item_classes.setText(classifier.HDF_FILE_CLASSES + self.file_classes)
-        #self.item_time_u.setText(classifier.HDF_FILE_TIME_U + self.file_time_u)
-        self.item_description.setText(classifier.HDF_FILE_DESCRIPTION + self.file_description)
-        self.item_task_count.setText(classifier.HDF_FILE_TASK_COUNT + str(int(self.file_task_count)))
-
 class project_description_new(QGroupBox):
-    def __init__(self, project_path=None):
+    def __init__(self, project_file=None, signal=None, project_path=None):
         super().__init__()
         self.project_path = project_path
+        self.file = project_file
+        self.signal = signal
         self.file_name = 'не задано'
         self.file_classes = 'не задано'
         self.file_time_c = 'не задано'
@@ -87,9 +47,27 @@ class project_description_new(QGroupBox):
         self.items_menu.addItem(self.item_time_u)
         self.items_menu.addItem(self.item_description)
         self.items_menu.addItem(self.item_task_count)
-        layout = QHBoxLayout()
+
+        self.btn_add = QPushButton("Добавить задачу")
+        self.btn_add.clicked.connect(self.on_add)
+
+        layout = QVBoxLayout()
         layout.addWidget(self.items_menu)
+        layout.addWidget(self.btn_add)
         self.setLayout(layout)
+    
+    def on_add(self):
+        task_to_add = QFileDialog.getOpenFileName()[0]
+        hdf = self.file
+        if task_to_add:
+            task_count = hdf.attrs[classifier.HDF_FILE_TASK_COUNT]
+            task_as_numpy = cv2.imread(task_to_add)
+            hdf.create_dataset(str(task_count), data=task_as_numpy)
+            hdf[str(task_count)].attrs[classifier.HDF_TASK_STATUS] = classifier.HDF_TASK_STATUS_0
+            hdf[str(task_count)].attrs[classifier.HDF_TASK_POLYGON_COUNT] = 0
+            hdf.attrs[classifier.HDF_FILE_TASK_COUNT] += 1
+            self.signal.emit()
+
 
     def update_description(self, hdf):
         self.file_name = hdf.attrs[classifier.HDF_FILE_NAME]
@@ -159,7 +137,6 @@ class main_window(QMainWindow):
         self.init_widgets()
         self.set_layouts()
         self.place_blocks()
-        #self.place_widgets()
         self.connect_ui()
         self.parse_projects_folder()
         #self.show_view_tab(project_path="/home/iakhmetev/Документы/8.3_version_2_data_labeling/__projects/123.hdf5")
@@ -180,29 +157,19 @@ class main_window(QMainWindow):
         self.tab_tasks_left_layout = QVBoxLayout()
         self.tab_tasks_right_layout = QVBoxLayout()
         self.tab_view_layout = QGridLayout()
-        #self.btn_group_open_layout = QVBoxLayout() #self.projects_control_layout = QVBoxLayout()
-        #self.btn_group_tasks_layout = QVBoxLayout() #self.tasks_control_layout = QVBoxLayout()
         self.view_project_control_layout = QVBoxLayout() #self.view_control_layout = QVBoxLayout()
-        #self.projects_control_layout = QVBoxLayout()
-        #self.tasks_control_layout = QVBoxLayout()
         self.view_control_layout = QVBoxLayout()
 
     def init_widgets(self):
         self.init_table()
         self.higher_control = higher_control(signal=self.signal_parse_projects)
         self.description = project_description_new()
-        #self.init_controls_projects()
-        #self.init_description()
-        #self.init_controls_tasks()
 
     def set_layouts(self):
         self.main_frame.setLayout(self.main_layout)
         self.tab_projects_group.setLayout(self.tab_projects_layout)
         self.tab_tasks_left_group.setLayout(self.tab_tasks_left_layout)
         self.tab_tasks_right_group.setLayout(self.tab_tasks_right_layout)
-        #self.btns_group_open.setLayout(self.btn_group_open_layout)
-        #self.higher_control.setLayout(self.projects_control_layout)
-        #self.btns_group_tasks.setLayout(self.btn_group_tasks_layout)
         self.tab_view.setLayout(self.tab_view_layout)
 
     def place_blocks(self):
@@ -211,40 +178,22 @@ class main_window(QMainWindow):
         self.tab.addTab(self.tab_view, "Просмотр")
 
         self.main_layout.addWidget(self.tab, 0, 0, 4, 1)
-        #self.main_layout.addWidget(self.btns_group_open, 0, 1)
         self.main_layout.addWidget(self.higher_control, 0, 1)
         self.main_layout.addWidget(self.description, 0, 1)
         self.show_higher_control()
-        
-        #self.main_layout.addWidget(self.btns_group_tasks, 0, 1)
-        #self.btns_group_tasks.setVisible(False)
-        
-
-    def place_widgets(self):
-        pass
-        #self.btn_group_open_layout.addWidget(self.btn_project_new)
-        #self.btn_group_open_layout.addWidget(self.btn_project_add)
-        #self.btn_group_open_layout.addWidget(self.btn_project_based)
-
-        #self.btn_group_tasks_layout.addWidget(self.btn_description)
-        #self.btn_group_tasks_layout.addWidget(self.btn_add_image)
 
     def connect_ui(self):
         self.signal_parse_tasks.connect(self.parse_tasks)
         self.signal_parse_projects.connect(self.parse_projects_folder)
         self.signal_open_project.connect(self.open_project_routine)
         self.signal_reopen_project.connect(self.reopen_project_routine)
-        #self.btn_project_new.clicked.connect(self.on_create_new_project_clicked)
-        #self.btn_project_new.clicked.connect(self.project_create_routine)
         self.tab.currentChanged.connect(self.show_tab)
         #self.btn_add_image.clicked.connect(self.on_add_new_task)
     
     def show_tab(self):
         if self.tab.currentWidget() == self.tab_split:
-            #self.show_controls_tasks_2()
             self.show_description()
         if self.tab.currentWidget() == self.tab_projects_area:
-            #self.show_controls_projects()
             self.show_higher_control()
 
     def show_description(self):
@@ -288,61 +237,13 @@ class main_window(QMainWindow):
         self.tab_tasks_right_area.setWidget(self.tab_tasks_right_group)
         #self.tab_view_widget.setWidget(self.tab_view_group)
 
-    #def init_controls_projects(self):
-    #    self.higher_control = higher_control(signal=self.signal_parse_projects)
-        #self.btns_group_open = QGroupBox()
-        #self.btn_project_new = QPushButton("Создать новый файл проекта")
-        #self.btn_project_add = QPushButton("Добавить проект из ...")
-        #self.btn_project_based = QPushButton("Создать проект на основе существующего")
-    
-    #def init_description(self):
-    #    self.description = project_description_new()
-
-    def init_controls_tasks(self):
-        self.btns_group_tasks = QGroupBox("Атрибуты проекта")
-        self.btns_group_tasks.setFixedSize(200,400)
-        self.btn_description = project_description()
-        #self.btn_description.setFixedSize(200,400)
-        self.btn_add_image = QPushButton("Добавить изображение")
-        self.btn_test = QPushButton("show")
-
-    def show_controls_tasks(self, description):
-        pass
-        #self.btns_group_open.setVisible(False)
-        #self.btns_group_tasks.setVisible(True)
-
-    def show_controls_tasks_2(self):
-        pass
-        #self.btns_group_open.setVisible(False)
-        #self.btns_group_tasks.setVisible(True)
-
-    def show_controls_projects(self):
-        pass
-        #self.btns_group_tasks.setVisible(False)
-        #self.btns_group_open.setVisible(True)
-
     def on_create_new_project_clicked(self):
         self.dialog = segflex_new_project.new_project_dialog(signal=self.signal_parse_projects)
         self.dialog.exec_()
 
-    def on_add_new_task(self):
-        task_to_add = QFileDialog.getOpenFileName()[0]
-        #match = re.search('[а-яА-Я]', task_to_add)
-        #assert match == None, 'cv2.imread need english file name'
-        if task_to_add:
-            with h5py.File(self.project_path, 'r+') as hdf:
-                group_srcs = hdf[classifier.HDF_GROUP_SRCS_NAME]
-                task_count = hdf.attrs[classifier.HDF_FILE_TASK_COUNT]
-                task_as_numpy = cv2.imread(task_to_add)
-                group_srcs.create_dataset(str(task_count), data=task_as_numpy)
-                group_srcs[str(task_count)].attrs[classifier.HDF_TASK_STATUS] = classifier.HDF_TASK_STATUS_0
-                group_srcs[str(task_count)].attrs[classifier.HDF_TASK_POLYGON_COUNT] = 0
-                hdf.attrs[classifier.HDF_FILE_TASK_COUNT] += 1
-            self.signal_parse_tasks.emit(self.project_path)
-
 
     def parse_projects_folder(self):
-        self.show_controls_projects()
+        #self.show_controls_projects()
         self.clear_table_layout(layout=self.tab_projects_layout)
         self.tab.setCurrentWidget(self.tab_projects_area)
         projects_list = os.listdir(classifier.PROJECTS_FOLDER_FULL_NAME)
@@ -419,7 +320,7 @@ class main_window(QMainWindow):
     def description_parse_routine(self):
         hdf = self.file
         #utils.clear_layout(self.tasks_control_layout)
-        self.description = project_description_new()
+        self.description = project_description_new(project_file=hdf, signal=self.signal_reopen_project)
         self.description.update_description(hdf)
         self.main_layout.addWidget(self.description, 0, 1)
         #self.tasks_control_layout.addWidget(self.description)
@@ -427,24 +328,6 @@ class main_window(QMainWindow):
     def descriptrion_reparse_routine(self):
         hdf = self.file
         self.description.update_description(hdf)
-
-    def btn_add_task_create(self):
-        self.btn_add_task = QPushButton("Добавить задачу")
-        self.btn_add_task.clicked.connect(self.on_add_new_task_new)
-        self.btn_group_tasks_layout.addWidget(self.btn_add_task)
-
-
-    def on_add_new_task_new(self):
-        task_to_add = QFileDialog.getOpenFileName()[0]
-        hdf = self.file
-        if task_to_add:
-            task_count = hdf.attrs[classifier.HDF_FILE_TASK_COUNT]
-            task_as_numpy = cv2.imread(task_to_add)
-            hdf.create_dataset(str(task_count), data=task_as_numpy)
-            hdf[str(task_count)].attrs[classifier.HDF_TASK_STATUS] = classifier.HDF_TASK_STATUS_0
-            hdf[str(task_count)].attrs[classifier.HDF_TASK_POLYGON_COUNT] = 0
-            hdf.attrs[classifier.HDF_FILE_TASK_COUNT] += 1
-            self.signal_reopen_project.emit()
 
     def task_parse_routine(self):
         hdf = self.file
